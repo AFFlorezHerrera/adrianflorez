99356704447dda66e25d3a4b05d7d9096de54061
Adrian Felipe Florez Herrera <151650260+AFFlorezHerrera@users.noreply.github.com>
2025-12-23 15:30:26 -0500

    Update content.tsx
    prueba de verificación error

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -61,96 +61,6 @@ const home: Home = {
   ),
 };
 
-const about: About = {
-  path: "/about",
-  label: "Sobre mi",
-  title: `About – ${person.name}`,
-  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
-  tableOfContent: {
-    display: true,
-    subItems: false,
-  },
-  avatar: {
-    display: true,
-  },
-  calendar: {
-    display: true,
-    link: "https://cal.com",
-  },
-  intro: {
-    display: true,
-    title: "Introducción",
-    description: (
-      <>
-        Actualmente me encuentro en un proceso de transición profesional hacia el mundo de la tecnología, donde podre poner en practica mis competencias en <Text as="span" size="xl" weight="strong">Excel avanzado, Power BI, SQL Server, Python </Text>
-y el uso de IA para optimizar análisis y procesos. 
-      </>
-    ),
-  },
-  work: {
-    display: true, // set to false to hide this section
-    title: "Experiencia laboral",
-    experiences: [
-      {
-        company: "Reforestadora DAGES SAS",
-        timeframe: "02/2025 - Actualidad",
-        role: "ingeniero / analista de datos",
-        achievements: [
-          <>
-            análisis de inventarios forestales a gran escala, construyendo modelos de datos y procesos de depuración que permitieron identificar zonas con 40% menos desarrollo productivo.
-          </>,
-          <>
-            Analizar y gestionar datos para la toma de decisiones en la plantación de pino.
-          </>,
-        ],
-        images: [
-          // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Pino Caribe",
-            width: 16,
-            height: 9,
-          },
-        ],
-      },
-      {
-        company: "Universidad Nacional de Colombia",
-        timeframe: "05/2019 - Actualidad",
-        role: "Ingeniero investigador",
-        achievements: [
-          <>
-            Investigador de cultivos enfocado en análisis de datos, pensamiento crítico y trabajo colaborativo, transformando información experimental en insights accionables para la toma de decisiones y mejora continua.
-          </>,
-        ],
-        images: [],
-      },
-    ],
-  },
-  studies: {
-    display: true, // set to false to hide this section
-    title: "Estudios",
-    institutions: [
-      {
-        name: "Universidad Nacional de Colombia",
-        description: <>Ingeniero agrónomo.</>,
-      },
-      {
-        name: "Soy Henry",
-        description: <>Analista de datos. </>,
-      },
-    ],
-  },
-  technical: {
-    display: true, // set to false to hide this section
-    title: "habilidades tecnicas",
-    skills: [
-      {
-        title: "Excel avanzado, Python, SQL, Power BI, R, Trello y clickup ",
-        },
-        ],
-      },
-    ],
-};
 
 const work: Work = {
   path: "/work",
@@ -161,4 +71,4 @@ const work: Work = {
   // All projects will be listed on the /home and /work routes
 };
 
-export { person, social, newsletter, home, about, work };
+export { person, social, newsletter, home, work };
